59fe582ee2b1ab41d6846684a169de16562ec18a
mikemartincode <miketmartin07@gmail.com>
2018-05-14 16:35:18 -0400

    Changing gitignore

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,6 +1,6 @@
-import App from "../src/containers/App";
 import React from "react";
 import renderer from "react-test-renderer";
+import App from "../src/containers/App";
 
 describe("Component: App", () => {
   it("should match its empty snapshot", () => {
